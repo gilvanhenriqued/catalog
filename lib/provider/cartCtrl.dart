a243da6bc8b63fb4d0b3d5942f578e129034b096
Gilvan Henrique <gilvanh.15@gmail.com>
2020-07-01 01:27:59 -0300

    cart controller list

--- lib/provider/cartCtrl.dart
+++ lib/provider/cartCtrl.dart
@@ -1,8 +1,35 @@
 import 'package:catalog/models/item.dart';
 import 'package:flutter/widgets.dart';
 
-class CartModel extends ChangeNotifier {
-  final List<Item> _items = [];
-  
-  
+class CartCtrl extends ChangeNotifier {
+  final List<Item> _items = [
+    Item(name: "Camisa vasual", price: 59.90),
+    Item(name: "Caução de veludo", price: 39.90),
+    Item(name: "Chapéu de vaqueiro", price: 49.90),
+    Item(name: "Sandália Nike", price: 89.90),
+    Item(name: "Óculos escuros", price: 69.90),
+    Item(name: "Bolsa de escola", price: 109.90),
+  ];
+
+  final List<Item> _selectedItems = [];
+
+  double total = 0;
+
+  void add(Item item) {
+    _selectedItems.add(item);
+    calculateTotal();
+  }
+
+  void remove(Item item) {
+    _selectedItems.remove(item);
+    calculateTotal();
+  }
+
+  calculateTotal() {
+    _selectedItems.forEach((i) {
+      total += i.price;  
+    });
+    notifyListeners();
+  }
+
 }
